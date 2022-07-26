57e140247caba049fdb105f3aa366fdedaacf1ab
jpattWPC <jpatten@westparkcom.net>
2022-07-26 12:25:42 -0500

    Add repo priorities

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,14 +21,21 @@ deb http://archive.ubuntu.com/ubuntu/ focal-updates universe
 deb http://archive.ubuntu.com/ubuntu/ focal-security universe
 EOM
 
+verbose "Installing wget and other necessities"
+apt update
+apt -y dist-upgrade
+apt install -y wget curl apt-transport-https python3-paramiko python3-pyinotify
+
 cat > /etc/apt/sources.list.d/petasan.list <<-EOM
 # PetaSAN updates
 deb http://archive.petasan.org/repo_v3/ petasan-v3 updates
 EOM
 
-verbose "Installing wget"
-
-apt install -y wget curl apt-transport-https python3-paramiko python3-pyinotify
+cat > /etc/apt/preferences.d/90-petasan <<-EOM
+Package: *
+Pin: release o=PetaSAN
+Pin-Priority: 700
+EOM
 
 verbose "Addding PetaSAN repo key"
 
@@ -36,8 +43,7 @@ wget -qO - http://archive.petasan.org/repo/release.asc | apt-key add -
 
 verbose "Installing PetaSAN kernel and utilities and updating all installed packages"
 
-apt update
-apt -y dist-upgrade
+
 apt -y -o Dpkg::Options::="--force-overwrite" install ceph-petasan cpupower linux-image-petasan petasan-firmware targetcli-fb lvm2 grub2 python3-ceph-argparse python3-ceph-common python3-ceph python3-cephfs python3-consul python3-rados python3-rbd python3-rgw python3-rtslib-fb radosgw rbd-fuse rbd-mirror rbd-nbd
 
 verbose "Setting PetaSAN kernel as default"
@@ -65,4 +71,4 @@ cp -a scripts/* /usr/local/bin
 systemctl enable rbdmap
 chmod +x stage2.sh
 
-warning "Bootstrap complete. Please reboot this server and run the `stage2.sh` script to continue."
+warning "Bootstrap complete. Please reboot this server and run the 'stage2.sh' script to continue."
